f29d0b5a9ad825defdb260fdf948fe9ce98e7137
mashabulkahfi <33783920+mashabulkahfi@users.noreply.github.com>
2018-12-13 01:18:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ connection.connect(function(err){
   console.log('connected to DB');
 });
 
+app.get('/pengiriman/:idkec',urlencodedParser, function(req, res){
+  var id = req.params.idkec;
+
+  connection.query('SELECT JNEREG, JNEOKE, WAHANA, TIKIREG, JT, SICEPAT FROM gw WHERE kecamatan_id=?', id, function(err, rows, fields) {
+    if (err) throw err;
+    res.status(200).send(rows[0]);
+  })
+});
+
 // app.use((req, res, next) => {
 //     res.status(200).json({
 //         message: 'It works wey'
